52ea3e8fe7b3dd4b6d93d15dc30223801bd1d2bb
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-11 20:30:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,7 +59,7 @@ def game_hash
     },
     :away => {
        :team_name => "Charlotte Hornets",
-      :colors =>  ["Turqoise", "Purple"],
+      :colors =>  ["Turquoise", "Purple"],
       :players => [{
          :player_name => "Jeff Adrien",
         :number => 4,
