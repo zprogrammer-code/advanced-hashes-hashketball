8235def2deb7b0c236727efc96aec27d6de91114
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-11 21:21:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -161,11 +161,14 @@ def player_numbers(team_name)
   value[:players].map{ |player|
   if value[:team_name] == "Brooklyn Nets"
     brooklyn_jerseys << player[:number]
+    return brooklyn_jerseys
   else value[:team_name] == "Charlotte Hornets"
     charlotte_jerseys << player[:number]
+    return charlotte_jerseys
   end
  }
   }
+  
 end
 binding.pry
   
